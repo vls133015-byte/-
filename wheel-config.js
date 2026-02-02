292a5fbfcaa90e49511e30fb6f8e84e699e8ab5c
vls133015-byte <vls133015@gmail.com>
2026-02-02 21:30:23 +0200

    Update wheel-config.js

--- wheel-config.js
+++ wheel-config.js
@@ -1,11 +1,1 @@
-﻿// Конфигурация сегментов колеса
-const wheelSegments = [
-  { fillStyle: '#ff0000', text: 'Красный' },
-  { fillStyle: '#000000', text: 'Чёрный' },
-  { fillStyle: '#ff0000', text: 'Красный' },
-  { fillStyle: '#000000', text: 'Чёрный' },
-  { fillStyle: '#ff0000', text: 'Красный' },
-  { fillStyle: '#000000', text: 'Чёрный' },
-  { fillStyle: '#ff0000', text: 'Красный' },
-  { fillStyle: '#000000', text: 'Чёрный' }
-];
+
